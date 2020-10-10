0d6098d895d22c747e35622a7da93025589a08ab
Sunil Chaudhary <1234huts@gmail.com>
2020-10-10 15:44:16 +0530

    Working on Clipboard.js :wrench:

--- src/js/components/copyToClipboard.js
+++ src/js/components/copyToClipboard.js
@@ -1,9 +1,28 @@
-// source https://codepen.io/shaikmaqsood/pen/XmydxJ
+// source https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
 // copy Text to Clipboard
-export default function copyToClipboard(element) {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($(element).text()).select();
-    document.execCommand("copy");
-    $temp.remove();
+export function copyToClipboard(text) {
+
+    text = document.querySelector('.codeText').textContent;
+    if (window.clipboardData && window.clipboardData.setData) {
+        // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
+        return clipboardData.setData("Text", text);
+
+    }
+    else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+        }
+        catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        }
+        finally {
+            document.body.removeChild(textarea);
+        }
+    }
 }
--- src/js/mine.js
+++ src/js/mine.js
@@ -1,12 +1,12 @@
 // Added No Conflict so kindly use jQuery at the start of the function & then $ for rest.
 $.noConflict();
 
-import copyToClipboard from "./components/copyToClipboard.js";
+import { copyToClipboard } from "./components/copyToClipboard.js";
 import { Alert } from "./components/alert.js";
 import { Button } from "./components/btn.js";
 import { Dropdown } from "./components/dropdown.js";
 
-copyToClipboard();
+jQuery(copyToClipboard);
 jQuery(Alert);
 jQuery(Button);
 jQuery(Dropdown);
