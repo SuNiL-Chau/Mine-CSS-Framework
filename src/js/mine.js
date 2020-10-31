a2fbd19b66f01ed40962e0e80059e59ae99dcaee
Sunil Chaudhary <1234huts@gmail.com>
2020-10-31 20:24:46 +0530

    Updated Docs,  button & table :hammer: :memo:

--- src/js/mine.js
+++ src/js/mine.js
@@ -1,12 +1,10 @@
 // Added No Conflict so kindly use jQuery at the start of the function & then $ for rest.
 $.noConflict();
 
-import { copyToClipboard } from "./components/copyToClipboard.js";
 import { Alert } from "./components/alert.js";
 import { Button } from "./components/btn.js";
 import { Dropdown } from "./components/dropdown.js";
 
-jQuery(copyToClipboard);
 jQuery(Alert);
 jQuery(Button);
 jQuery(Dropdown);
